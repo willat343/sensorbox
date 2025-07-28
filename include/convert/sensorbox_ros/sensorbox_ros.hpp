26fc932b40a9ee68814900b88a99d017e74a32b3
William Talbot <william.talbot97@gmail.com>
2025-07-28 17:41:22 +0200

    moved sensorbox_ros to this repo from convert

---
+++ include/convert/sensorbox_ros/sensorbox_ros.hpp
@@ -0,0 +1,8 @@
+#ifndef CONVERT_SENSORBOX_ROS_SENSORBOX_ROS_HPP
+#define CONVERT_SENSORBOX_ROS_SENSORBOX_ROS_HPP
+
+#include "convert/sensorbox_ros/imu.hpp"
+#include "convert/sensorbox_ros/pose.hpp"
+#include "convert/sensorbox_ros/pose_twist.hpp"
+
+#endif
