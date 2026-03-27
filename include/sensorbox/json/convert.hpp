59d64b43033b42c3274955deed358dd2e65e994c
William Talbot <william.talbot97@gmail.com>
2026-03-27 18:07:55 +0100

    added conversion of binary contacts to json

--- include/sensorbox/json/convert.hpp
+++ include/sensorbox/json/convert.hpp
@@ -3,11 +3,14 @@
 
 #include <nlohmann/json.hpp>
 
+#include "sensorbox/contact.hpp"
 #include "sensorbox/sensor.hpp"
 
 namespace sensorbox {
 
 void from_json(const nlohmann::json& j, SensorType& type);
+
+void to_json(nlohmann::json& j, const ContactClassifications& contact_classifications);
 
 }
 
--- include/sensorbox/json/impl/convert.impl.hpp
+++ include/sensorbox/json/impl/convert.impl.hpp
@@ -1,6 +1,8 @@
 #ifndef SENSORBOX_JSON_IMPL_CONVERT_IMPL_HPP
 #define SENSORBOX_JSON_IMPL_CONVERT_IMPL_HPP
 
+#include <cppbox/time.hpp>
+
 #include "sensorbox/impl/sensorbox.hpp"
 #include "sensorbox/json/impl/convert.hpp"
 
@@ -8,6 +10,15 @@ namespace sensorbox {
 
 SENSORBOX_INLINE void from_json(const nlohmann::json& j, SensorType& type) {
     type = SensorType(j.get<std::string>());
+}
+
+SENSORBOX_INLINE void to_json(nlohmann::json& j, const ContactClassifications& contact_classifications) {
+    j = nlohmann::json::object();
+    j["timestamp"] = cppbox::to_nsec(contact_classifications.timestamp());
+    j["values"] = nlohmann::json::array();
+    for (const auto& [key, classification] : contact_classifications.classifications()) {
+        j["values"].push_back({{"key", key}, {"value", classification}});
+    }
 }
 
 }
