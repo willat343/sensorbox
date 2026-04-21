c3b0b066f7b5282cd1efc80bf9645cd28011d6f3
William Talbot <william.talbot97@gmail.com>
2026-04-21 17:17:35 +0200

    minor fixes to math in documentation

--- include/sensorbox/pose.hpp
+++ include/sensorbox/pose.hpp
@@ -69,12 +69,12 @@ public:
     explicit PoseMeasurement();
 
     /**
-     * @brief Construct a Pose Measurement, of child_frame_ (C) in frame_ (F), i.e. \f$\mathbf{T}_{FC}\f$.
+     * @brief Construct a Pose Measurement, of child_frame_ (C) in frame_ (F), i.e. \f$ \mathbf{T}_{FC} \f$.
      *
      * @param timestamp_ timestamp
      * @param frame_ reference frame in which the measurement is made, e.g. map_frame
      * @param child_frame_ reference frame that has been measured, e.g. body_frame
-     * @param pose_ pose of child_frame_ in frame_, \f$\mathbf{T}_{FC}\f$
+     * @param pose_ pose of child_frame_ in frame_, \f$ \mathbf{T}_{FC} \f$
      */
     explicit PoseMeasurement(const Timestamp& timestamp_, const std::string& frame_, const std::string& child_frame_,
             const Pose& pose_);
@@ -94,8 +94,8 @@ public:
      *      T_{F,N} = T_{F,C} * T_{C,N}
      * \f]
      *
-     * @param new_child_frame new child frame \f$N\f$
-     * @param T_C_N pose of new child frame in the current child frame \f$T_{C,N}\f$
+     * @param new_child_frame new child frame \f$ N \f$
+     * @param T_C_N pose of new child frame in the current child frame \f$ T_{C,N} \f$
      * @return PoseMeasurement
      */
     PoseMeasurement transform_to_new_child_frame(const std::string& new_child_frame, const Pose& T_C_N) const;
@@ -107,8 +107,8 @@ public:
      *      T_{N,C} = T_{N,F} * T_{F,C}
      * \f]
      *
-     * @param new_frame new frame \f$N\f$
-     * @param T_N_F pose of current frame in the new frame \f$T_{N,F}\f$
+     * @param new_frame new frame \f$ N \f$
+     * @param T_N_F pose of current frame in the new frame \f$ T_{N,F} \f$
      * @return PoseMeasurement
      */
     PoseMeasurement transform_to_new_frame(const std::string& new_frame, const Pose& T_N_F) const;
@@ -120,10 +120,10 @@ public:
      *      T_{NF,NC} = T_{NF,F} * T_{F,C} * T_{C,NC}
      * \f]
      *
-     * @param new_frame new frame \f$NF\f$
-     * @param new_child_frame new child frame \f$NC\f$
-     * @param T_NF_F pose of current frame in the new frame \f$T_{NF,F}\f$
-     * @param T_C_NC pose of new child frame in the current child frame \f$T_{C,N}\f$
+     * @param new_frame new frame \f$ NF \f$
+     * @param new_child_frame new child frame \f$ NC \f$
+     * @param T_NF_F pose of current frame in the new frame \f$ T_{NF,F} \f$
+     * @param T_C_NC pose of new child frame in the current child frame \f$ T_{C,N} \f$
      * @return PoseMeasurement
      */
     PoseMeasurement transform_to_new_frames(const std::string& new_frame, const std::string& new_child_frame,
